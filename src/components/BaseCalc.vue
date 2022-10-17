f333bdfddbd9d0d48e0c9084a179fbf856b510fa
Denis Masot <denis.masot@gmail.com>
2022-10-17 12:00:21 +0200

    add files

---
+++ src/components/BaseCalc.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref  } from "vue"
+
+const counter = ref(0)
+
+const increaseCounter = () => {
+    counter.value++
+}
+
+</script>
+
+
+<template>
+  <h1>Base :</h1>
+    {{counter}}
+    <button @click="increaseCounter">+</button>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { translate } from '@/i18n'
+import i18n from '@/main'
+import HeaderCmp from './components/HeaderComponents.vue'
+
+const langs = i18n.global.availableLocales;
+
+function changeLang(val: string) {
+  if(typeof i18n.global.locale.value !== "string") return;
+  i18n.global.locale.value = val;
+}
+</script>
+
+<template>
+  <h3>Header</h3>
+  <button
+    v-for="(lang, i) in langs"
+    :key="i"
+    @click="changeLang(lang)"
+  >{{lang}}</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
+
